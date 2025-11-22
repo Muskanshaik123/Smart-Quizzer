ec9e0149bf5d29acbe282fa71d02d7671fecc268
Muskanshaik123 <153289875+Muskanshaik123@users.noreply.github.com>
2025-11-22 21:17:03 +0530

    Delete backend directory

--- backend/generate.js
+++
@@ -1,7 +0,0 @@
-const crypto = require('crypto');
-
-// Generate a random 64-character hex string
-const jwtSecret = crypto.randomBytes(64).toString('hex');
-console.log('🔐 Your JWT Secret:');
-console.log(jwtSecret);
-console.log('\n📋 Copy this into your .env file as JWT_SECRET=');
